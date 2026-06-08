02c17e43c3f479b13e7fb0dd9f4d55ba2f36b58c
Faheer Hakim <r0820158@student.thomasmore.be>
2026-06-08 21:23:04 +0200

    feat: voeg IcecreamOrder component toe met het basis bestelformulier

---
+++ src/components/icecreamOrder.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="order-form">
+<h1>Ben & Jerry's Order Formulier</h1>
+<div class="form">
+<h2>Contact gegevens</h2>
+<div class="bezoeker-info">
+<label class="placeholder" for="name">Naam</label>
+<input class="inputs inputPadding" type="text" id="name" name="name" v-model="name"></div>
+
+<label class="placeholder" for="name">E-mail</label>
+<input class="inputs inputPadding" type="text" id="email" v-model="email"></div>
+
+<label class="placeholder" for="name">Gsm nummer</label>
+<input class="inputs inputPadding" type="text" id="phone" name="name" v-model="phone"></div>
+
+<div class="form">
+<h2>Ben & Jerry's Bestelling</h2>
+<div class="placeholder" for="icecream">
+<input class ="inputs inputPadding" type="text" id="donut"  v-model="coneFlavor">
+</div>
+</div>
+
+
+
+</div>
+</template>
