d5486397aafa387cef9226caad79cb2540507f13
Faheer Hakim <r0820158@student.thomasmore.be>
2026-06-11 11:23:22 +0200

    model icecream  is toegevoegd aan de scene

--- src/components/icecreamConfig.vue
+++ src/components/icecreamConfig.vue
@@ -7,5 +7,13 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+let icecream;
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load('../../models/icecream.glb', (gltf) => {
+    icecream = gltf.scene;
+    console.log(gltf);
+    scene.add(gltf.scene);
+});
 
 </script>
