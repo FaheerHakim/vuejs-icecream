af96608ca88f9ef288bd63c261a24cfc76c4e5f3
Faheer Hakim <r0820158@student.thomasmore.be>
2026-06-11 13:10:52 +0200

    OrbitControls is geimporteert

--- src/components/icecreamConfig.vue
+++ src/components/icecreamConfig.vue
@@ -1,6 +1,7 @@
 <script setup>
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
 
